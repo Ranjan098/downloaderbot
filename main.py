7c6045eeb42cac2c5d3cfd29143c83f4310cea54
Ranjan098 <144373098+Ranjan098@users.noreply.github.com>
2023-11-14 00:08:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ from get_video_info import get_video_attributes, get_video_thumb
 
 API_ID = 12606917
 API_HASH = "f25113b8c17dca6fa7abda53a86bd4f7"
-BOT_TOKEN = "6705040975:AAER_R1UpXDIa06YWvuzo0LJrTO7KUVW5K4"
+BOT_TOKEN = "6705040975:AAET-xG-am2cMKK7Q8A1r1iZ91JY2I9sUGo"
 AUTH_USERS = 5318243282
 sudo_users = [5318243282]
 bot = Client(
